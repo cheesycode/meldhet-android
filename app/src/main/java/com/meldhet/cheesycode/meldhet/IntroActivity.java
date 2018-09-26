94c96480f085b9f62eefda17b7d1d37e582e360a
Sascha Worms <sworms@avans.nl>
2018-09-26 09:05:43 +0200

    Initial commit

---
+++ app/src/main/java/com/meldhet/cheesycode/meldhet/IntroActivity.java
@@ -0,0 +1,89 @@
+package com.meldhet.cheesycode.meldhet;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.github.paolorotolo.appintro.model.SliderPage;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class IntroActivity extends AppIntro2 {
+
+    SharedPreferences prefs = null;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        prefs = getSharedPreferences("com.mycompany.myAppName", MODE_PRIVATE);
+        if (!prefs.getBoolean("firstrun", true)) {
+            Intent mainactivity = new Intent(IntroActivity.this, MainActivity.class);
+            mainactivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            this.startActivity(mainactivity);
+            finish();
+        }
+        askForPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+
+
+        setSwipeLock(false);
+        SliderPage sliderPage = new SliderPage();
+        sliderPage.setTitle("Welkom");
+        sliderPage.setDescription("Leuk dat je onze app gebruikt om de wereld groener te maken");
+        sliderPage.setImageDrawable(R.drawable.banner);
+        sliderPage.setBgColor(getResources().getColor(R.color.colorAquaDark));
+        addSlide(AppIntroFragment.newInstance(sliderPage));
+        setSwipeLock(false);
+        sliderPage.setTitle("Vuil of Kapot");
+        sliderPage.setDescription("Elke dag lopen we langs tientalle dingen die vuil of kapot zijn, waar niemand iets van weet.");
+        sliderPage.setImageDrawable(R.drawable.banner);
+        sliderPage.setBgColor(getResources().getColor(R.color.colorGrapeFruitDark));
+        addSlide(AppIntroFragment.newInstance(sliderPage));
+        setSwipeLock(false);
+        sliderPage.setTitle("Ga Opzoek");
+        sliderPage.setDescription("Met deze app kun jij op zoek naar de dingen die jij belangrijk vindt.");
+        sliderPage.setImageDrawable(R.drawable.banner);
+        sliderPage.setBgColor(getResources().getColor(R.color.colorLavanderDark));
+        addSlide(AppIntroFragment.newInstance(sliderPage));
+        setSwipeLock(false);
+        sliderPage.setTitle("Klik op een Categorie");
+        sliderPage.setDescription("En maak een foto. Wij doen de rest, wij brengen de gemeente op de hoogte en zorgen dat alles geregeld moet worden. ");
+        sliderPage.setImageDrawable(R.drawable.banner);
+        sliderPage.setBgColor(getResources().getColor(R.color.colorGrassDark));
+        addSlide(AppIntroFragment.newInstance(sliderPage));
+        setSwipeLock(false);
+
+        prefs.edit().putBoolean("firstrun", false).commit();
+
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        Intent mainactivity = new Intent(IntroActivity.this, MainActivity.class);
+        mainactivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.startActivity(mainactivity);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent mainactivity = new Intent(IntroActivity.this, MainActivity.class);
+        mainactivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        this.startActivity(mainactivity);
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+
+    }
+}
---
+++ app/src/main/java/com/meldhet/cheesycode/meldhet/MainActivity.java
@@ -0,0 +1,96 @@
+package com.meldhet.cheesycode.meldhet;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private String mCurrentPhotoPath;
+    private String currentMeldingType;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.titleimage);
+    }
+
+    public void onImageOrTextClick(View view) {
+
+        if(view instanceof TextView){
+            currentMeldingType = ((TextView) view).getText().toString();
+        }
+        else if(view instanceof ImageView)
+        {
+            currentMeldingType = ((ImageView) view).getContentDescription().toString();
+        }
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this, "Failed to create picture, please reset permissions" , Toast.LENGTH_LONG).show();
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                Toast.makeText(this, R.string.fotoinstructie , Toast.LENGTH_LONG).show();
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+
+                imageView.setImageBitmap(myBitmap);
+            }
+        }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "MeldingOp" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+}
+
